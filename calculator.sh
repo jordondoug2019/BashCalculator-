d31f0abf392a4b2e6ba3d14078bb84532f3377bb
Jordon <jdouglas2015@gmail.com>
2024-08-11 16:02:40 +0000

    updated conditonals and while loop

--- calculator.sh
+++ calculator.sh
@@ -3,34 +3,34 @@
 
 # Requirements:
 
-x=1
-while [ "$x" -le 5 ];
-do
 #The script should prompt the user to enter two integers.
-	echo "Welcome to the Bash Calculator. Enter q to quit"
-	echo "Welcome to the Bash Calculator. Please enter the 1st Number and press return"
-	read int1
+echo "Welcome to the Bash Calculator. Please enter the 1st Number and press return"
+read int1
+
 
 	if ! [[ "$int1" =~ /^[a-zA-Z]+$ ]]
 	then
 		echo "Please enter a number"
+		read int1
 
-	else 
-		echo "Please enter the 2nd Number"
-		read int2
 	fi
+
+echo "Please enter the 2nd Number"
+read int2
+
 
 	if ! [[ "$int2" =~ /^[a-zA-Z]+$ ]]
 	then
 		echo "Please enter a number"
+		read int2
+	fi
 
-	else 
 	
-        	echo "Select Operation to perform : +.-.*./"
-       		read operation
+echo "Select Operation to perform : +.-.*./"
+read operation
 #The script should prompt the user to choose an operation: +,-,*,/
 #Use conditionals ('if`, `elif`, `else') to perform the chosen operation.
-	fi
+
 	if [ "$operation" = "+" ]
 	then 
         	echo "$(( $int1 + $int2 ))"
@@ -52,7 +52,6 @@ do
 			echo "$(( $int1 / $int2 ))"
         	fi
 	fi
-done
 #Display the result of the operation.
 #Handle division by zero with an appropriate error message.
  
